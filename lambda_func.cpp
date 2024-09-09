2523471d9cf7c081205a03131b1a1e2d7227d349
Devansh Khetan <devanshkhetan@Devanshs-MacBook-Air.local>
2024-09-09 11:41:49 +0530

    Lambda Func

---
+++ lambda_func.cpp
@@ -0,0 +1,36 @@
+// DK09
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+#include <utility>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <iterator>
+#include <climits>
+
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+     #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    auto sum = [](int x){return x+2;};
+    cout<<sum(2);
+    return 0;
+}
+
+
+
